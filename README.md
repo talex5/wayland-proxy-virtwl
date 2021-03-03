5f8a1910b4e2b0e8c260b1e79942f4b14201f56a
Thomas Leonard <talex5@gmail.com>
2021-03-03 18:50:47 +0000

    Add --wayland-display argument
    Allow the user to specify where to listen for connections.
    Also, if the path already exists then try connecting to it to see if an
    existing service is already running, and refuse to start if so.

--- README.md
+++ README.md
@@ -7,21 +7,43 @@ It proxies all requests to the host Wayland compositor using the virtwl Linux mo
 See https://alyssa.is/using-virtio-wl/ for some background.
 
 Since regular guest memory cannot be shared with the host, it allocates a shadow buffer from the host and copies the frame data into that.
-Wayland can also use graphics memory directly, which should avoid the copy.
-
-`tests/test.ml` is a simple test application that uses the virtwl kernel interface directly (without the proxy), avoiding any copying.
+Wayland can also use graphics memory directly, which should avoid the copy, but this is not yet supported.
 
 It can also be used to make changes to the protocol messages.
 e.g. using `--tag` will add a prefix to window titles.
 
 It is similar to the [sommelier][] proxy from ChromiumOS, but written in OCaml.
+It adds support for the primary selection, and aims to be easier to modify and less segfaulty.
 It uses the [ocaml-wayland][] library.
+
+It is able to proxy Evince and Firefox at least.
+
+## Installation
+
+I use the following systemd file to run it
+(in `~/.local/share/systemd/user/wayland-virtlw-proxy.service`):
+
+```
+[Unit]
+Description=Wayland-virtwl-proxy
+
+[Service]
+ExecStart=/path/to/wayland-proxy-virtwl --tag="[my-vm] " --wayland-display wayland-0
+
+[Install]
+WantedBy=default.target
+```
+
+## Using virtwl directly
+
+`tests/test.ml` is a simple test application that uses the virtwl kernel interface directly
+(without the proxy), avoiding any copying.
 
 ## TODO
 
-So far, it has only been tested with `evince` (GNOME's PDF viewer) and provides only the exact protocol versions that it requires. Also:
-
 - Find alternative to private `caml_unix_mapped_alloc`.
+- Only copy the buffer regions that have changed.
+- Fork a subprocess for each connection, like sommelier.
 
 [sommelier]: https://chromium.googlesource.com/chromiumos/platform2/+/main/vm_tools/sommelier/
 [ocaml-wayland]: https://github.com/talex5/ocaml-wayland
--- virtwl_proxy.ml
+++ virtwl_proxy.ml
@@ -1,7 +1,5 @@
 open Lwt.Syntax
 
-let socket_path = "/run/user/1000/wayland-1"
-
 let rec listen ~config socket =
   let* (client, _addr) = Lwt_unix.accept socket in
   Log.info (fun f -> f "New connection");
@@ -13,20 +11,40 @@ let () =
   (* Logs.(set_level (Some Info)); *)
   Printexc.record_backtrace true
 
-let main tag args =
+let is_listening path =
+  let s = Unix.(socket PF_UNIX SOCK_STREAM 0) in
+  Fun.protect ~finally:(fun () -> Unix.close s) @@ fun () ->
+  match Unix.connect s (Unix.ADDR_UNIX path) with
+  | () -> true
+  | exception Unix.Unix_error(Unix.ECONNREFUSED, _, _) -> false
+  | exception ex ->
+    Log.warn (fun f -> f "Error testing socket %S: %a" path Fmt.exn ex);
+    false
+
+let main tag wayland_display args =
   let config = { Config.tag } in
-  Lwt_main.run begin
-    let listening_socket = Unix.(socket PF_UNIX SOCK_STREAM 0) in
-    if Sys.file_exists socket_path then Unix.unlink socket_path;
-    Unix.bind listening_socket (Unix.ADDR_UNIX socket_path);
-    Unix.listen listening_socket 5;
-    Log.info (fun f -> f "Listening on %S" socket_path);
-    Lwt.async (fun () -> listen ~config (Lwt_unix.of_unix_file_descr listening_socket));
-    Unix.putenv "WAYLAND_DISPLAY" socket_path;
-    let child = Lwt_process.open_process ("", Array.of_list args) in
-    let* _ = child#status in
-    Lwt.return ()
-  end
+  let socket_path = Wayland.Unix_transport.socket_path ~wayland_display () in
+  let existing_socket = Sys.file_exists socket_path in
+  if existing_socket && is_listening socket_path then (
+   `Error (false, Fmt.strf "A server is already listening on %S!" socket_path)
+  ) else (
+    if existing_socket then Unix.unlink socket_path;
+    Lwt_main.run begin
+      let listening_socket = Unix.(socket PF_UNIX SOCK_STREAM 0) in
+      Unix.bind listening_socket (Unix.ADDR_UNIX socket_path);
+      Unix.listen listening_socket 5;
+      Log.info (fun f -> f "Listening on %S" socket_path);
+      Lwt.async (fun () -> listen ~config (Lwt_unix.of_unix_file_descr listening_socket));
+      Unix.putenv "WAYLAND_DISPLAY" wayland_display;
+      match args with
+      | [] ->
+        fst (Lwt.wait ())
+      | args ->
+        let child = Lwt_process.open_process ("", Array.of_list args) in
+        let* _ = child#status in
+        Lwt.return (`Ok ())
+    end
+  )
 
 open Cmdliner
 
@@ -37,14 +55,21 @@ let tag =
     ~doc:"Tag to prefix to window titles"
     ["tag"]
 
+let wayland_display =
+  Arg.value @@
+  Arg.(opt string) "wayland-1" @@
+  Arg.info
+    ~doc:"Name or path of socket to listen on"
+    ["wayland-display"]
+
 let args =
   Arg.value @@
-  Arg.(pos_all string) ["evince"] @@
+  Arg.(pos_all string) [] @@
   Arg.info
     ~doc:"Sub-command to execute"
     []
 
-let virtwl_proxy = Term.(const main $ tag $ args)
+let virtwl_proxy = Term.(ret (const main $ tag $ wayland_display $ args))
 
 let term_exit (x : unit Term.result) = Term.exit x
 
