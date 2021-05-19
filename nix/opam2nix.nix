0d01391bfde65a630dc0da37ceaa5fe6372a1d7c
Thomas Leonard <talex5@gmail.com>
2021-05-19 18:07:12 +0100

    Use an older release of opam2nix for NixOS 20.09
    Error was:
    
        error: attribute 'ocamlPackages_4_12' missing,
        at /nix/store/9qcbizv0fs1zml6n5y28v6hir5ip3x2v-rh6fkkjchgcbbvjxk2bwhrx68lqy8rp9-source/nix/default.nix:2:36

--- nix/opam2nix.nix
+++ nix/opam2nix.nix
@@ -1,1 +1,13 @@
-import (builtins.fetchTarball "https://github.com/timbertson/opam2nix/archive/v1.tar.gz") {}
+# Latest version (for NixOS 21.05):
+# import (builtins.fetchTarball "https://github.com/timbertson/opam2nix/archive/v1.tar.gz") {}
+
+# For NixOS 20.09 (stable):
+import (
+  with import <nixpkgs> {};
+  fetchFromGitHub {
+    owner = "timbertson";
+    repo = "opam2nix";
+    rev = "90f8d627501f39e16e20c22161ec7d0aec10af32";
+    sha256 = "1d60adr1z26mdn4hp146263f5fhb66yhwdfsf9m9lgpybbra7gkh";
+  }
+) {}
