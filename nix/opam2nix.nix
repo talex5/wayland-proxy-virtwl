1be5a8f853bcca342bc7676f27f04018787a038c
Thomas Leonard <talex5@gmail.com>
2021-12-04 10:41:01 +0000

    Update opam2nix for NixOS 21.11

--- nix/opam2nix.nix
+++ nix/opam2nix.nix
@@ -1,16 +1,27 @@
-# Latest version (for NixOS 21.05):
+# Latest version
 # import (builtins.fetchTarball "https://github.com/timbertson/opam2nix/archive/v1.tar.gz") {}
 
-# For NixOS 21.05 (stable):
+# For NixOS 21.11
 import (
   with import <nixpkgs> {};
   fetchFromGitHub {
     owner = "timbertson";
     repo = "opam2nix";
-    rev = "48e98dce385cee4a615445249d4c0aaa58596872";
-    sha256 = "1drw254961xqg2gz7pbkgpc81gxwhq11bzdp83kzc0sdj5hixsxd";
+    rev = "2184246b338b98abd301e4aeca6e8a9d5f5ce80c";
+    sha256 = "1l9bf9svpbgnfyfcxrrpajgilvlsdvfmyayij59fx0rc9p8bhhz5";
   }
 ) {}
+
+# For NixOS 21.05
+# import (
+#   with import <nixpkgs> {};
+#   fetchFromGitHub {
+#     owner = "timbertson";
+#     repo = "opam2nix";
+#     rev = "48e98dce385cee4a615445249d4c0aaa58596872";
+#     sha256 = "1drw254961xqg2gz7pbkgpc81gxwhq11bzdp83kzc0sdj5hixsxd";
+#   }
+# ) {}
 
 # For NixOS 20.09:
 # import (
