9dc459c6465edfa10488f28563a01293f52f66fb
Demi Marie Obenour <demi@invisiblethingslab.com>
2024-09-15 14:13:54 -0400

    Use 3-space indent in C code
    No functional change intended.

--- relay_stubs.c
+++ relay_stubs.c
@@ -22,34 +22,34 @@ bool validate_planes(uint32_t formats[static 4], uint64_t modifiers[static 4],
                      uint32_t width, uint32_t height,
                      uint32_t plane_count)
 {
-  if (plane_count > 4)
-    return false; /* too many */
+   if (plane_count > 4)
+      return false; /* too many */
 
-  for (uint32_t i = 0; i < plane_count; ++i) {
-    switch (modifiers[i]) {
-    case I915_FORMAT_MOD_Y_TILED_CCS:
-    case I915_FORMAT_MOD_Yf_TILED_CCS:
-      if (i != 1 || plane_count != 2)
-        return false; /* bad! */
-      if (modifiers[0] != modifiers[i] - 2)
-        return false; /* mismatch between plane 0 and plane 1 */
-      if (stride[0] % 1024 != 0 || stride[1] != stride[0] / 8)
-        return false; /* bad stride */
-      if (height % 512 != 0)
-        return false; /* bad height */
-      /* TODO: check other requirements */
-      break;
-    case I915_FORMAT_MOD_Y_TILED_GEN12_RC_CCS:
-      if (i != 1 || plane_count != 2)
-        return false; /* bad! */
-      if (modifiers[0] != I915_FORMAT_MOD_Y_TILED)
-        return false; /* bad! */
-      if (stride[0] % (4096 * 4))
-        return false; /* main surface pitch not multiple of 4 Y tile widths */
-      break;
-    }
-  }
-  return true; /* TODO catch more bad cases */
+   for (uint32_t i = 0; i < plane_count; ++i) {
+      switch (modifiers[i]) {
+      case I915_FORMAT_MOD_Y_TILED_CCS:
+      case I915_FORMAT_MOD_Yf_TILED_CCS:
+         if (i != 1 || plane_count != 2)
+            return false; /* bad! */
+         if (modifiers[0] != modifiers[i] - 2)
+            return false; /* mismatch between plane 0 and plane 1 */
+         if (stride[0] % 1024 != 0 || stride[1] != stride[0] / 8)
+            return false; /* bad stride */
+         if (height % 512 != 0)
+            return false; /* bad height */
+         /* TODO: check other requirements */
+         break;
+      case I915_FORMAT_MOD_Y_TILED_GEN12_RC_CCS:
+         if (i != 1 || plane_count != 2)
+            return false; /* bad! */
+         if (modifiers[0] != I915_FORMAT_MOD_Y_TILED)
+            return false; /* bad! */
+         if (stride[0] % (4096 * 4))
+            return false; /* main surface pitch not multiple of 4 Y tile widths */
+         break;
+      }
+   }
+   return true; /* TODO catch more bad cases */
 }
 
 /*   UINT32_MAX * UINT32_MAX + UINT32_MAX
@@ -61,14 +61,14 @@ bool validate_planes(uint32_t formats[static 4], uint64_t modifiers[static 4],
  */
 static uint64_t mul_32_to_64(uint32_t a, uint32_t b)
 {
-  return (uint64_t)a * (uint64_t)b;
+   return (uint64_t)a * (uint64_t)b;
 }
 
 static uint32_t div_round_up(uint32_t num, uint32_t denom)
 {
-  if (denom < 1)
-    abort();
-  return (uint32_t)(((uint64_t)num + (uint64_t)denom - UINT64_C(1)) / denom);
+   if (denom < 1)
+      abort();
+   return (uint32_t)(((uint64_t)num + (uint64_t)denom - UINT64_C(1)) / denom);
 }
 
 /**
@@ -83,102 +83,102 @@ static uint32_t div_round_up(uint32_t num, uint32_t denom)
 static int64_t
 get_fd_size(long fd)
 {
-  if (fd < 0 || fd > INT_MAX) {
-    caml_fatal_error("Junk file descriptor passed to validate_fd");
-  }
+   if (fd < 0 || fd > INT_MAX) {
+      caml_fatal_error("Junk file descriptor passed to validate_fd");
+   }
 
-  off_t untrusted_raw_size = lseek((int)fd, 0, SEEK_END);
-  if (untrusted_raw_size < 0 || untrusted_raw_size > INT64_MAX) {
-    /* lseek() failed or size too large for int64_t */
-    return -1;
-  }
+   off_t untrusted_raw_size = lseek((int)fd, 0, SEEK_END);
+   if (untrusted_raw_size < 0 || untrusted_raw_size > INT64_MAX) {
+      /* lseek() failed or size too large for int64_t */
+      return -1;
+   }
 
-  return (int64_t)untrusted_raw_size;
+   return (int64_t)untrusted_raw_size;
 }
 
 CAMLprim int64_t
 wayland_proxy_virtwl_check_fd_offset_native(value fd)
 {
-  return get_fd_size(Long_val(fd));
+   return get_fd_size(Long_val(fd));
 }
 
 CAMLprim value
 wayland_proxy_virtwl_check_fd_offset_byte(value fd)
 {
-  return caml_copy_int64(get_fd_size(Long_val(fd)));
+   return caml_copy_int64(get_fd_size(Long_val(fd)));
 }
 
 static const struct drm_format_info *
 get_format(uint32_t untrusted_format)
 {
-  const struct drm_format_info *const info = drm_format_info(untrusted_format);
-  if (info == NULL) {
-    /* Unknown format, cannot validate. */
-    return NULL;
-  }
+   const struct drm_format_info *const info = drm_format_info(untrusted_format);
+   if (info == NULL) {
+      /* Unknown format, cannot validate. */
+      return NULL;
+   }
 
-  if (info->hsub < 1 || info->vsub < 1 || info->num_planes < 0 || info->num_planes > 4) {
-    caml_fatal_error("Corrupt description for format");
-  }
+   if (info->hsub < 1 || info->vsub < 1 || info->num_planes < 0 || info->num_planes > 4) {
+      caml_fatal_error("Corrupt description for format");
+   }
 
-  if (info->is_color_indexed) {
-    return NULL; /* Color index cannot be used. */
-  }
+   if (info->is_color_indexed) {
+      return NULL; /* Color index cannot be used. */
+   }
 
-  if (drm_format_info_block_height(info, 0) != 1) {
-    /* Multiple pixels per vertical block.  It is unclear whether the
-     * stride should be multiplied by the block height or not. */
-    return NULL;
-  }
+   if (drm_format_info_block_height(info, 0) != 1) {
+      /* Multiple pixels per vertical block.  It is unclear whether the
+       * stride should be multiplied by the block height or not. */
+      return NULL;
+   }
 }
 
 static const struct drm_format_info *
 validate_format(uint32_t untrusted_format, int32_t untrusted_width, int32_t untrusted_height)
 {
-  if (untrusted_width < 1 || untrusted_height < 1) {
-    return NULL; /* No pixels. */
-  }
+   if (untrusted_width < 1 || untrusted_height < 1) {
+      return NULL; /* No pixels. */
+   }
 
-  if (untrusted_width > 16384 || untrusted_height > 6144) {
-    return NULL; /* Too wide, too tall, or both. */
-  }
+   if (untrusted_width > 16384 || untrusted_height > 6144) {
+      return NULL; /* Too wide, too tall, or both. */
+   }
 
-  const struct drm_format_info *const info = get_format(untrusted_format);
-  if (info == NULL) {
-    return NULL;
-  }
+   const struct drm_format_info *const info = get_format(untrusted_format);
+   if (info == NULL) {
+      return NULL;
+   }
 
-  if (untrusted_width % info->hsub) {
-    return NULL; /* Width not multiple of subsampled block size, violating Vulkan valid usage. */
-  }
+   if (untrusted_width % info->hsub) {
+      return NULL; /* Width not multiple of subsampled block size, violating Vulkan valid usage. */
+   }
 
-  if (untrusted_height % info->vsub) {
-    return NULL; /* Height not multiple of subsampled block size, violating Vulkan valid usage. */
-  }
+   if (untrusted_height % info->vsub) {
+      return NULL; /* Height not multiple of subsampled block size, violating Vulkan valid usage. */
+   }
 
-  /* Check that the block width is valid. */
-  const unsigned int block_width = drm_format_info_block_width(info, 0);
-  if (block_width < 1) {
-    return NULL;
-  }
+   /* Check that the block width is valid. */
+   const unsigned int block_width = drm_format_info_block_width(info, 0);
+   if (block_width < 1) {
+      return NULL;
+   }
 
-  /* Check that the block height is valid. */
-  const unsigned int block_height = drm_format_info_block_height(info, 0);
-  if (block_height < 1) {
-    return NULL;
-  }
+   /* Check that the block height is valid. */
+   const unsigned int block_height = drm_format_info_block_height(info, 0);
+   if (block_height < 1) {
+      return NULL;
+   }
 
-  if (untrusted_width % block_width) {
-    /* Image does not have a whole number of horizontal blocks. */
-    return NULL;
-  }
+   if (untrusted_width % block_width) {
+      /* Image does not have a whole number of horizontal blocks. */
+      return NULL;
+   }
 
-  if (untrusted_height % block_height) {
-    /* Image does not have a whole number of vertical blocks. */
-    return NULL;
-  }
+   if (untrusted_height % block_height) {
+      /* Image does not have a whole number of vertical blocks. */
+      return NULL;
+   }
 
-  return info;
+   return info;
 }
 
 static bool
@@ -187,61 +187,61 @@ validate_shm(int32_t untrusted_offset,
              int32_t const untrusted_stride,
              uint32_t const untrusted_format)
 {
-  /* Offset can't be negative */
-  if (untrusted_offset < 0) {
-    return false;
-  }
+   /* Offset can't be negative */
+   if (untrusted_offset < 0) {
+      return false;
+   }
 
-  /* This checks that untrusted_width and untrusted_height are both at least 1
-   * and that untrusted_width * untrusted_height * 8 does not overflow.
-   * If these checks fail, info will be NULL, which the functions below catch. */
-  const struct drm_format_info *info = validate_format(untrusted_format, untrusted_width, untrusted_height);
-  if (info == NULL) {
-    return false;
-  }
+   /* This checks that untrusted_width and untrusted_height are both at least 1
+    * and that untrusted_width * untrusted_height * 8 does not overflow.
+    * If these checks fail, info will be NULL, which the functions below catch. */
+   const struct drm_format_info *info = validate_format(untrusted_format, untrusted_width, untrusted_height);
+   if (info == NULL) {
+      return false;
+   }
 
-  /* Stride must not be less than width.  Since width must be in [1, 16384],
-   * stride must be greater than 0. */
-  if (untrusted_stride < untrusted_width) {
-    return false;
-  }
+   /* Stride must not be less than width.  Since width must be in [1, 16384],
+    * stride must be greater than 0. */
+   if (untrusted_stride < untrusted_width) {
+      return false;
+   }
 
-  /* Limit supported strides to 16384 * 8 until there is a need for more. */
-  if (untrusted_stride > 16384UL * 8) {
-    return false;
-  }
+   /* Limit supported strides to 16384 * 8 until there is a need for more. */
+   if (untrusted_stride > 16384UL * 8) {
+      return false;
+   }
 
-  /* Multi-plane formats are badly specified, so block them.
-   * TODO: support them properly. */
-  if (info->num_planes != 1) {
-    return false;
-  }
+   /* Multi-plane formats are badly specified, so block them.
+    * TODO: support them properly. */
+   if (info->num_planes != 1) {
+      return false;
+   }
 
-  if (info->char_per_block[0] < 1) {
-    /* The format requires a non-linear modifier, and shm buffers always use
-     * linear modifiers. This is an error. */
-    return false;
-  }
+   if (info->char_per_block[0] < 1) {
+      /* The format requires a non-linear modifier, and shm buffers always use
+       * linear modifiers. This is an error. */
+      return false;
+   }
 
-  if ((untrusted_stride % info->char_per_block[0]) != 0) {
-    /* Row does not include a whole number of blocks.  This is an error. */
-    return false;
-  }
+   if ((untrusted_stride % info->char_per_block[0]) != 0) {
+      /* Row does not include a whole number of blocks.  This is an error. */
+      return false;
+   }
 
-  if ((untrusted_offset % info->char_per_block[0]) != 0) {
-    /* Offset is not a whole number of blocks.  This is an error. */
-    return false;
-  }
+   if ((untrusted_offset % info->char_per_block[0]) != 0) {
+      /* Offset is not a whole number of blocks.  This is an error. */
+      return false;
+   }
 
-  uint64_t min_pitch = drm_format_info_min_pitch(info, 1, (uint32_t)untrusted_width);
+   uint64_t min_pitch = drm_format_info_min_pitch(info, 1, (uint32_t)untrusted_width);
 
-  if (min_pitch < 1) {
-    /* This means a bug in the DRM format handling code. */
-    caml_fatal_error("BUG: invalid pitch returned by drm_format_info_min_pitch");
-  }
+   if (min_pitch < 1) {
+      /* This means a bug in the DRM format handling code. */
+      caml_fatal_error("BUG: invalid pitch returned by drm_format_info_min_pitch");
+   }
 
-  /* Check that the stride is sufficient. */
-  return untrusted_stride >= min_pitch;
+   /* Check that the stride is sufficient. */
+   return untrusted_stride >= min_pitch;
 }
 
 static const struct drm_format_info *
@@ -250,50 +250,50 @@ validate_fd(long fd, uint32_t untrusted_offset,
             uint32_t stride, uint32_t format, uint64_t modifiers,
             uint32_t plane_idx)
 {
-  caml_fatal_error("TODO");
+   caml_fatal_error("TODO");
 }
 
 CAMLprim value
 validate_shm_native(int32_t offset, int32_t width, int32_t height, int32_t stride, int32_t format)
 {
-  return Val_bool(validate_shm(offset, width, height, stride, (uint32_t)format));
+   return Val_bool(validate_shm(offset, width, height, stride, (uint32_t)format));
 }
 
 CAMLprim value
 validate_shm_byte(value offset, value width, value height, value stride, value format)
 {
-  return Val_bool(validate_shm(Int32_val(offset), Int32_val(width), Int32_val(height),
-                               Int32_val(stride), (uint32_t)Int32_val(format)));
+   return Val_bool(validate_shm(Int32_val(offset), Int32_val(width), Int32_val(height),
+                                Int32_val(stride), (uint32_t)Int32_val(format)));
 }
 
 CAMLprim bool validate_fd_native(intnat fd, int32_t offset, int32_t width, int32_t height,
                                  int32_t stride, int32_t format, int64_t modifiers,
                                  int32_t plane_idx)
 {
-  if (fd < 0 || fd > INT_MAX)
-    return false;
-  return validate_fd((int)fd, (uint32_t)offset, width, height, stride,
-                     (uint32_t)format, (uint64_t)modifiers, (uint64_t)plane_idx);
+   if (fd < 0 || fd > INT_MAX)
+      return false;
+   return validate_fd((int)fd, (uint32_t)offset, width, height, stride,
+                      (uint32_t)format, (uint64_t)modifiers, (uint64_t)plane_idx);
 }
 
 CAMLprim value
 validate_fd_byte(value *argv, int argc)
 {
-  if (argc != 8)
-    caml_fatal_error("wrong arity");
-  return Val_bool(validate_fd_native(Long_val(argv[0]),
-                                     (uint32_t)Int32_val(argv[1]),
-                                     Int32_val(argv[2]),
-                                     Int32_val(argv[3]),
-                                     Int32_val(argv[4]),
-                                     (uint32_t)Int32_val(argv[5]),
-                                     (uint64_t)Int64_val(argv[6]),
-                                     (uint32_t)Int32_val(argv[7])));
+   if (argc != 8)
+      caml_fatal_error("wrong arity");
+   return Val_bool(validate_fd_native(Long_val(argv[0]),
+                                      (uint32_t)Int32_val(argv[1]),
+                                      Int32_val(argv[2]),
+                                      Int32_val(argv[3]),
+                                      Int32_val(argv[4]),
+                                      (uint32_t)Int32_val(argv[5]),
+                                      (uint64_t)Int64_val(argv[6]),
+                                      (uint32_t)Int32_val(argv[7])));
 }
 
 /*
  * Local Variables:
- * c-basic-offset: 2
+ * c-basic-offset: 3
  * indent-tabs-mode: nil
  * c-file-style: "linux"
  * End:
